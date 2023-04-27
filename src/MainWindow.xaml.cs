255dc0767d859a4073632f67bb41a0c7d78170e3
lemonek <72318911+lemonekq@users.noreply.github.com>
2023-04-27 14:20:03 +0200

    mslogin working
    drut to repair

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -1,27 +1,71 @@
 ﻿using betterlauncher_cs.modules;
+using CmlLib.Core.Auth;
+using CmlLib.Core.Auth.Microsoft;
+using CmlLib.Core.Auth.Microsoft.MsalClient;
+using Microsoft.Identity.Client;
 using System;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
+using MessageBox = System.Windows.Forms.MessageBox;
 
 namespace betterlauncher_cs
 {
     public partial class MainWindow : Window
     {
         public static string version = "0.0.0";
+        public static string mslogin_clientid = "c774e229-39fa-48ea-ad91-b0fb7e75945d";
 
         public MainWindow()
         {
             // post-app run lag spike on animations delay
-            Thread.Sleep(100);
+            Thread.Sleep(2500 / Environment.ProcessorCount);
             InitializeComponent();
         }
 
-        private async void Window_Initialized(object sender, EventArgs e) // async for l8r?
+        private async void Window_Initialized(object sender, EventArgs e)
         {
             configmanager.Prepare();
             handler.Height = 35; handler.Width = 35; handler.RadiusX = 35; handler.RadiusY = 35;
             navbar.Opacity = 0.0f;
             contenthandler.Opacity = 0.0f;
+            app = await MsalMinecraftLoginHelper.BuildApplicationWithCache(mslogin_clientid);
         }
+
+        public static IPublicClientApplication app;
+        public static JavaEditionLoginHandler loginHandler;
+        public static MSession session;
+        public static CancellationTokenSource loginCancel;
+        public static void loginSuccess(MSession session)
+        {
+            // session.accesstoken <=
+        }
+
+        public static async Task LoginAndShowResultOnUI(JavaEditionLoginHandler loginHandler)
+        {
+            try
+            {
+                var session = await loginHandler.LoginFromOAuth();
+                loginSuccess(session.GameSession);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        static async void mslogin()
+        {
+            if (app == null) return;
+
+            loginHandler = new LoginHandlerBuilder()
+                .ForJavaEdition()
+                .WithMsalOAuth(app, factory => factory.CreateWithEmbeddedWebView())
+            .Build();
+
+            await LoginAndShowResultOnUI(loginHandler);
+        }
+
+        private void mslogin_init(object sender, System.Windows.Input.MouseButtonEventArgs e) => mslogin();
     }
 }
