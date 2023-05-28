3361f3065f92828f7d4e9c7d5e42f1669a2e49e7
lemonek <72318911+lemonekq@users.noreply.github.com>
2023-05-28 20:53:52 +0200

    rumcajs update: big animations update

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -14,6 +14,18 @@ namespace betterlauncher_cs
             windowhandler_bg.Height = 20; windowhandler_bg.Width = 20;
             windowhandler_mask1.Height = 20; windowhandler_mask1.Width = 20;
             windowhandler_mask2.Height = 20; windowhandler_mask2.Width = 20;
+
+            handler_left.Opacity = 0.0f;
+            season_bg.Opacity = 0.0f;
+            season_bg_mask.Opacity = 0.0f;
+            season_bg_title.Opacity = 0.0f;
+            season_bg_title_bg.Opacity = 0.0f;
+            season_bg_button_github.Opacity = 0.0f;
+            launch_button.Opacity = 0.0f;
+            launch_progressbar_bg.Opacity = 0.0f;
+            launch_progressbar_bar.Opacity = 0.0f;
+            launch_progressbar_text.Opacity = 0.0f;
+            settings_button.Opacity = 0.0f;
         }
     }
 }
