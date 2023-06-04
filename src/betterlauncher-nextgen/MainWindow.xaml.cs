fca22d0c85c0ce4179055a9998e499efa5e3d5ce
lemonek <72318911+lemonekq@users.noreply.github.com>
2023-06-04 21:36:31 +0200

    triangles v1

--- src/betterlauncher-nextgen/MainWindow.xaml.cs
+++ src/betterlauncher-nextgen/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
 
 namespace betterlauncher_nextgen
 {
@@ -20,14 +24,19 @@ namespace betterlauncher_nextgen
             handler_bg_mask.Height = 50; handler_bg_mask.Width = 50; handler_bg_mask_rotatetransform.Angle = 90; handler_bg_mask.Margin = new Thickness(0, 250, 0, 0); handler_bg_mask.Opacity = 0.0f;
             versionhandler.Opacity = 0.0f;
             seasonhandler.Opacity = 0.0f;
+            launchbutton.Opacity = 0.0f;
+            seasonhandler_2_0.Opacity = 0.0f;
+            seasonhandler_title.Opacity = 0.0f;
             
             rotatebg();
         }
         public void rotatebg()
         {
             Random BackgroundRandom = new Random();
-            int BackgroundOutput = BackgroundRandom.Next(1, BackgroundCount);
+            int BackgroundOutput = BackgroundRandom.Next(1, BackgroundCount + 1);
             string imagePath = $"pack://application:,,,/Resources/bg-season-{BackgroundOutput}.jpg";
+            BitmapImage BackgroundImage = new BitmapImage(new Uri(imagePath));
+            seasonhandler_image.ImageSource = BackgroundImage;
         }
 
         private void seasonhandler_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
@@ -39,6 +48,14 @@ namespace betterlauncher_nextgen
             double OffsetY = mousePosition.Y / 10000;
 
             seasonhandler_translatetransform.X = OffsetX- 0.10; seasonhandler_translatetransform.Y = OffsetY - 0.10;
+        }
+
+        private void Rectangle_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
         }
     }
 }
