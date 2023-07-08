a421a6fb33780c476ebc43456da8bb871aa749af
lemonek <72318911+lemonekq@users.noreply.github.com>
2023-07-08 12:53:13 +0200

    push to work on laptop

--- src/betterlauncher-nextgen/Windows/AccountWindow.xaml.cs
+++ src/betterlauncher-nextgen/Windows/AccountWindow.xaml.cs
@@ -2,6 +2,7 @@
 using XboxAuthNet.OAuth;
 using System.Windows;
 using CmlLib.Core.Auth.Microsoft;
+using System.Security.Principal;
 
 namespace betterlauncher_nextgen.Windows
 {
@@ -21,14 +22,14 @@ namespace betterlauncher_nextgen.Windows
         private async void handler_accountselector_microsoft_Click(object sender, RoutedEventArgs e)
         {
             // mslogin :>
+            this.Topmost = false;
+            MainWindow.Instance.Topmost = false;
             try
             {
-                var loginHandler = MainWindow.LoginHandler;
-                // var session = await loginHandler.AuthenticateInteractively();
-                var authenticator = loginHandler.CreateAuthenticatorWithNewAccount();
-                authenticator.AddForceMicrosoftOAuthForJE(oauth => oauth.Interactive());
-                authenticator.AddXboxAuthForJE(xbox => xbox.Basic());
-                authenticator.AddForceJEAuthenticator();
+                var authenticator = MainWindow.LoginHandler.CreateAuthenticator(account, default);
+                authenticator.AddMicrosoftOAuthForJE(oauth => oauth.Interactive()); // Microsoft OAuth
+                authenticator.AddXboxAuthForJE(xbox => xbox.Basic()); // XboxAuth
+                authenticator.AddJEAuthenticator(); // JEAuthenticator
                 var session = await authenticator.ExecuteForLauncherAsync();
             }
             catch (Exception ex)
